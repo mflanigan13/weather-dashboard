0a7b33b60d8da758a284053115813c29e4875cca
mflanigan <mflanigantwualumn@gmail.com>
2021-12-22 13:10:30 -0600

    added javascript for current weather and 5 day

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,221 @@
-var cityFormEl=document.querySelector("#city-search-form");
-var cityInputEl=document.querySelector("#city");
-var weatherContainerEl=document.querySelector("#current-weather-container");
+var cityFormEl = document.querySelector("#city-search-form");
+var cityInputEl = document.querySelector("#city");
+var weatherContainerEl = document.querySelector("#current-weather-container");
 var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
-// function to get and display the current conditions 
+var cities = [];
 
-    // date and city name
-    
-    // temp
+// function to get city information from weather form
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if (city) {
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({ city });
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+};
 
-    // wind
+// save search to local storage
+var saveSearch = function () {
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
 
-    //humidity
+// get the city weather from OpenWeather One Call API
+var getCityWeather = function (city) {
+    var apiKey = "844421298d794574c100e3409cee0499";
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-    // UV index
+    fetch(apiURL).then(function (response) {
+        response.json().then(function (data) {
+            displayWeather(data, city);
+        });
+    });
+};
 
-// function to obtain the five day forecast and display to html
+// display the weather in the current weather container
+var displayWeather = function (weather, searchCity) {
+    //clear old content
+    weatherContainerEl.textContent = "";
+    citySearchInputEl.textContent = searchCity;
 
-// search city feature
+    //create date element
+    var currentDate = document.createElement("span");
+    currentDate.textContent =
+        " (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+    citySearchInputEl.appendChild(currentDate);
 
-// save city to local storage
+    //create an image element
+    var weatherIcon = document.createElement("img");
+    weatherIcon.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+    );
+    citySearchInputEl.appendChild(weatherIcon);
 
-// click on a city in the search history
+    //create a span element to hold temperature data
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    temperatureEl.classList = "list-group-item";
+
+    //create a span element to hold Humidity data
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+    humidityEl.classList = "list-group-item";
+
+    //create a span element to hold Wind data
+    var windSpeedEl = document.createElement("span");
+    windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+    windSpeedEl.classList = "list-group-item";
+
+    //append to container
+    weatherContainerEl.appendChild(temperatureEl);
+    weatherContainerEl.appendChild(humidityEl);
+    weatherContainerEl.appendChild(windSpeedEl);
+
+    var lat = weather.coord.lat;
+    var lon = weather.coord.lon;
+    getUvIndex(lat, lon);
+};
+
+var getUvIndex = function(lat,lon){
+    var apiKey = "844421298d794574c100e3409cee0499"
+    var apiURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            displayUvIndex(data)
+
+        });
+    });
+}
+
+// display the UV index for current weather
+var displayUvIndex = function(index){
+    var uvIndexEl = document.createElement("div");
+    uvIndexEl.textContent = "UV Index: "
+    uvIndexEl.classList = "list-group-item"
+
+    uvIndexValue = document.createElement("span")
+    uvIndexValue.textContent = index.value
+
+    if(index.value <=2){
+        uvIndexValue.classList = "favorable"
+    }else if(index.value >2 && index.value<=8){
+        uvIndexValue.classList = "moderate "
+    }
+    else if(index.value >8){
+        uvIndexValue.classList = "severe"
+    };
+
+    // append to elements
+    uvIndexEl.appendChild(uvIndexValue);
+    weatherContainerEl.appendChild(uvIndexEl);
+}
+
+
+// five day weather section
+var get5Day = function (city) {
+    var apiKey = "844421298d794574c100e3409cee0499";
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
+
+    fetch(apiURL).then(function (response) {
+        response.json().then(function (data) {
+            display5Day(data);
+        });
+    });
+};
+
+// display the five day weather section
+var display5Day = function (weather) {
+    forecastContainerEl.textContent = "";
+    forecastTitle.textContent = "5-Day Forecast:";
+
+    var forecast = weather.list;
+    for (var i = 5; i < forecast.length; i = i + 8) {
+        var dailyForecast = forecast[i];
+
+        var forecastEl = document.createElement("div");
+        forecastEl.classList = "card bg-info text-light m-2";
+
+        //create date element
+        var forecastDate = document.createElement("h5");
+        forecastDate.textContent = moment
+            .unix(dailyForecast.dt)
+            .format("MMM D, YYYY");
+        forecastDate.classList = "card-header text-center";
+        forecastEl.appendChild(forecastDate);
+
+        //create an image element
+        var weatherIcon = document.createElement("img");
+        weatherIcon.classList = "card-body text-center";
+        weatherIcon.setAttribute(
+            "src",
+            `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`
+        );
+
+        //append to forecast card
+        forecastEl.appendChild(weatherIcon);
+
+        //create temperature span
+        var forecastTempEl = document.createElement("span");
+        forecastTempEl.classList = "card-body text-center";
+        forecastTempEl.textContent = "Temperature: " + dailyForecast.main.temp + " °F";
+
+        //append to forecast card
+        forecastEl.appendChild(forecastTempEl);
+        
+        // create wind span
+        var forecastWindEl = document.createElement("span");
+        forecastWindEl.classList = "card-body text-center";
+        forecastWindEl.textContent = "Wind: " + dailyForecast.wind.speed + " MPH";
+        console.log(dailyForecast);
+
+        //append to forecast card
+        forecastEl.appendChild(forecastWindEl);
+
+        var forecastHumEl = document.createElement("span");
+        forecastHumEl.classList = "card-body text-center";
+        forecastHumEl.textContent = "Humidity: " + dailyForecast.main.humidity + "  %";
+
+        //append to forecast card
+        forecastEl.appendChild(forecastHumEl);
+
+        //append to five day container
+        forecastContainerEl.appendChild(forecastEl);
+    }
+};
+
+// display the past searched cities
+var pastSearch = function(pastSearch){
+
+    pastSearchEl = document.createElement("button");
+    pastSearchEl.textContent = pastSearch;
+    pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
+    pastSearchEl.setAttribute("data-city",pastSearch)
+    pastSearchEl.setAttribute("type", "submit");
+
+    pastSearchButtonEl.prepend(pastSearchEl);
+};
+
+var pastSearchHandler = function(event){
+    var city = event.target.getAttribute("data-city")
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+    }
+};
+
 
 // event listeners
+cityFormEl.addEventListener("submit", formSubmitHandler);
+pastSearchButtonEl.addEventListener("click", pastSearchHandler);
 
-// call functions
