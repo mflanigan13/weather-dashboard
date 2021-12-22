6308374984a27f891300cf4d18f94c580b58e552
mflanigan <mflanigantwualumn@gmail.com>
2021-12-22 11:05:21 -0600

    outlined js with comments

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,27 @@ var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+// function to get and display the current conditions 
+
+    // date and city name
+    
+    // temp
+
+    // wind
+
+    //humidity
+
+    // UV index
+
+// function to obtain the five day forecast and display to html
+
+// search city feature
+
+// save city to local storage
+
+// click on a city in the search history
+
+// event listeners
+
+// call functions
